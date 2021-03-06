3bc88231e1edfa69eab519ec4e3c517a5b1a3393
Diego Neri <diego@diegoneri.com>
2021-03-05 22:29:22 -0300

    Finaliza o projeto NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            string nomeCompleto;
+            string nomeCatalogo;
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Veja seu nome em diferentes perspectivas.");
+            Console.WriteLine("-----------------------------------------\n");
+
+            Console.Write("Digite seu primeiro nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome: ");
+            sobrenome = Console.ReadLine(); 
+
+            nomeCompleto = $"{nome} {sobrenome}";
+            nomeCatalogo = $"{sobrenome.ToUpper()}, {nome}";
+
+            Console.WriteLine();
+            Console.WriteLine($"Nome completo: {nomeCompleto}");
+            Console.WriteLine($"Nome de catálogo: {nomeCatalogo}");
+        }
+    }
+}
